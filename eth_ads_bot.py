0b21ff0859537608d42b24518a9f02dbf659b2c5
Dikamsi Young <youngudochi15@gmail.com>
2020-09-17 16:33:52 +0100

    Update eth_ads_bot.py

--- eth_ads_bot.py
+++ eth_ads_bot.py
@@ -6,14 +6,9 @@ api_id = 'api_id from my.telegram.org'
 api_hash = 'api_hash from my.telegram.org'  
 
 # Bot Information
-bot_name = 'BitcoinClick_bot'
-view_ad_command  = '/visit'
+bot_name = 'ETH_Ads_uk_bot'
+view_ad_command  = '/viewads'
 view_ad_button_number = 0   # Usually the first button
-
-# Browser Information
-OS_LIST = ['win32', 'linux64', 'mac64']   #available options from Google
-os_version = OS_LIST[0]    #Set OS version from OS_LIST
-chrome_version = '85.0.4183.87'    #Set Chrome Version
 
 ### Setting up Telethon
 _ = get_ipython().getoutput('pip3 install telethon')
@@ -36,13 +31,13 @@ _ = await client.connect()
 _ = await client.send_message(bot_name, '/start')
 _ = await client.connect()
 reply = await client.get_messages(bot_name, 1)
-reply[0].message
+reply[view_ad_button_number].message
 
 ### For ETH Ads Bot
 async def send_message():
     time.sleep(35)
     await client.connect()
-    await client.send_message('ETH_Ads_bot', '/viewads')
+    await client.send_message(bot_name, view_ad_command)
     print('Done!')
     
 ### Start
